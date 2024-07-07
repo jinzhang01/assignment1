cbb9533f814993f941fd264a746d081350a2fd11
jinzhang01 <144280070+jinzhang01@users.noreply.github.com>
2024-07-06 18:16:53 -0700

    draft win page

---
+++ components/Win.js
@@ -0,0 +1,19 @@
+import { View, Text, Image, Button } from 'react-native'
+import React from 'react'
+
+const Win = () => {
+  return (
+    <View>
+      <Text>You guessed correct!</Text>
+      <Text>Attempts used: {}</Text>
+      <Image 
+        source={{ uri: "https://picsum.photos/id/14/100/100" }} 
+        style={{ width: 100, height: 100 }} 
+        alt="win" 
+      />
+      <Button title='New Game' onPress={() =>console.log('New Game')} />  
+    </View>
+  )
+}
+
+export default Win
--- screens/Game.js
+++ screens/Game.js
@@ -1,12 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import GuessNum from '../components/GuessNum'
+import Win from '../components/Win'
 
 const Game = () => {
   return (
     <View>
       <Text>Game</Text>
       <GuessNum />
+        <Win />
     </View>
   )
 }
